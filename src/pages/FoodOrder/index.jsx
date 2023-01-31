57da95bb4094b335909c47744582940c31ddf757
Emanuelle <manufritzenpro@gmail.com>
2023-01-31 17:53:09 -0300

    Food Order

--- src/pages/FoodOrder/index.jsx
+++ src/pages/FoodOrder/index.jsx
@@ -1,9 +1,13 @@
 import {Container} from './styles';
+import { Header } from '../../components/Header';
+import { Footer } from '../../components/Footer';
 
 export function FoodOrder(){
     return(
         <Container>
-            
+            <Header/>
+
+            <Footer/>
         </Container>
     )
 }
